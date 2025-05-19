6d60fc2cdd48d712fecce1e82c3e31d994b53ba7
Sahitya Chekka <chekkasahitya@gmail.com>
2025-05-19 18:36:14 -0500

    adding reqs and changing port

--- app.py
+++ app.py
@@ -126,6 +126,6 @@ def result():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get the port from the environment
+    port = int(os.environ.get("PORT", 5001))  # Get the port from the environment
     app.run(host="0.0.0.0", port=port)        # Bind to 0.0.0.0 for Render
 
